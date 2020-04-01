efc34ad383355135ecdbfa038d472cd6eb0ef38f
Gerald Manipon <pymonger@gmail.com>
2020-04-01 14:46:06 -0700

    add script to build and deploy

---
+++ build_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# build all boxes
+for i in base mozart metrics grq factotum cont_int; do
+  cd $i
+  vagrant plugin install vagrant-vbguest
+  vagrant up
+  vagrant package --base $i
+  vagrant cloud auth login
+  vagrant cloud publish -d "HySDS CentOS7 $i box." --version-description "Version 4.0.0" -r -c $(md5 -q package.box) -C md5 hysds/$i 4.0.0 virtualbox package.box
+  vagrant destroy -f
+  cd ..
+done
