061a29fe6b2aecd18e4d1dee7ff95db1e4670f0d
Gerald Manipon <pymonger@gmail.com>
2020-04-01 15:03:10 -0700

    run without confirmation

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,7 +7,7 @@ for i in base mozart metrics grq factotum cont_int; do
   vagrant up
   vagrant package --base $i
   vagrant cloud auth login
-  vagrant cloud publish -d "HySDS CentOS7 $i box." --version-description "Version 4.0.0" -r -c $(md5 -q package.box) -C md5 hysds/$i 4.0.0 virtualbox package.box
+  vagrant cloud publish -f -d "HySDS CentOS7 $i box." --version-description "Version 4.0.0" -r -c $(md5 -q package.box) -C md5 hysds/$i 4.0.0 virtualbox package.box
   vagrant destroy -f
   cd ..
 done
